f449b74dadf05acbfb66650892edba503c650a55
Peter Shepherd <jpsheph@clemson.edu>
2025-10-20 00:02:14 -0400

    feat: improved posture analysis with shoulder length and add some framework

--- PostureChecker/PoseDetector.py
+++ PostureChecker/PoseDetector.py
@@ -25,13 +25,17 @@ while True:
         left_shoulder = landmarks[11]
         right_shoulder = landmarks[12]
 
+        left_shoulder_x = left_shoulder.x
+        right_shoulder_x = right_shoulder.x
+        shoulder_Length = left_shoulder_x - right_shoulder_x
         shoulder_y = (left_shoulder.y + right_shoulder.y) / 2
         nose_y = nose.y
 
-        posture_ratio = shoulder_y - nose_y
+        posture_ratio = (shoulder_y - nose_y) / shoulder_Length
 
+        cv2.putText(frame, f"Shoulder Length: {shoulder_Length:.4f}", (30, 110), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
         cv2.putText(frame, f"Posture Ratio: {posture_ratio:.4f}", (30, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-        if posture_ratio > 0.24:
+        if posture_ratio > 0.75:
             cv2.putText(frame, "Good posture", (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
         else:
             cv2.putText(frame, "Bad posture!", (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
